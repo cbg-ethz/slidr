51fc1b437469d167f1e46f6e0530739f22b7bc48
anamus90 <anamus90@gmail.com>
2019-03-12 17:13:19 +0100

    Plot for figure 3 for cancer specific hits

---  Scripts/Res1_plots.R
+++  Scripts/Res1_plots.R
@@ -1,4 +1,12 @@
+# Plotting figure for pan-cancer hits
+
+library(cowplot)
+library(ggplot2)
+library(viridis)
+library(dplyr)
+
 # Load the summary data for the plots
+# load("~/Downloads/Slidr_Results/Pan_cancer_8pc/summary_data.Rdata")
 load("~/Downloads/Slidr_Results/Pan_cancer_8pc/summary_data.Rdata")
 
 # Load the hit list
@@ -10,12 +18,12 @@ barplot_df$canc_type <- factor(barplot_df$canc_type,
                                labels = c("Autonomic ganglia", "Bone", "Breast", "CNS", "Endometrium", "Blood", "Kidney", "Large intestine", "Liver", "Lung", "Oesophagus",
                                           "Ovary", "Pancreas", "Pleura", "Skin", "Soft tissue", "Stomach", "UADT", "Urinary tract"))
 
-coloursSites <- c("#CC99BB", "#AA4488", "#771155", "#77AADD",
+colour_sites <- c("#CC99BB", "#AA4488", "#771155", "#77AADD",
                   "#4477AA", "#114477", "#77CCCC", "#44AAAA", "#117777", "#88CCAA",
                   "#44AA77", "#117744", "#DDDD77", "#AAAA44", "#777711", "#DDAA77",
                   "#AA7744", "#774411", "#DD7788")
 
-c <- ggplot(barplot_df, aes(fill = canc_type, x = reorder(driver_gene,-tot_samples), y = n_samples)) +
+a <- ggplot(barplot_df, aes(fill = canc_type, x = reorder(driver_gene,-tot_samples), y = n_samples)) +
   geom_bar(stat = "identity") +
   theme_bw() +
   ylab("Number of mutated cell lines") +
@@ -35,43 +43,21 @@ c <- ggplot(barplot_df, aes(fill = canc_type, x = reorder(driver_gene,-tot_sampl
         legend.title = element_text(vjust = 1, size=12,colour="#525252"),
         legend.text = element_text(size=10,colour="#525252"),
         legend.key.size = unit(0.5, "cm"))+
-  scale_fill_manual(values = coloursSites)+
+  scale_fill_manual(values = colour_sites)+
   scale_y_continuous(breaks = seq(0, 280, by = 40))
 
-# Heatmap of important hits
-# top_sl_pairs    <- paste(hits_pancan$driver_gene[1:100], hits_pancan$sl_partner_gene[1:100], sep = "_")
-top_drivers   <- c("ACVR2A", "APC", "BRAF", "COL5A1","CTNNB1", "DMRTA1", "KRAS", "MTAP", "NOTCH1", "SYNE1") #"RPL22", COL24A1
-hm_summary_df <- summary_df %>% filter(driver_gene %in% top_drivers)
+# Loading the causal results for plotting confounders and heatmap
+load("~/Downloads/Slidr_Results/Pan_cancer_8pc/causal_res.RData")
+# Loading the summary dataframe after removing confounders
+load("~/Downloads/Slidr_Results/Pan_cancer_8pc/filt_summary_data.Rdata")
 
-d <- ggplot(hm_summary_df, aes(x = sl_partner_gene, y = canc_type)) +
-  geom_tile(aes(fill = -log(as.numeric(as.character(mut_pvalue))))) +
-  facet_grid(.~ factor(driver_gene), scales = "free_x", space = "free_x") +
-  scale_fill_viridis(option="D", begin = 0.1, end = 0.9, alpha = 0.9) +
-  theme_bw() +
-  ylab("Primary sites") +
-  xlab("SL partner genes") +
-  labs(fill = "-log(p-value)")+
-  theme(panel.grid= element_blank(),
-        axis.ticks = element_line(size=0.5,color="#525252"),
-        axis.line = element_line(colour="#525252"),
-        axis.text.y=element_text(angle=0, vjust=0.5, hjust=1,size=10,colour="#525252"),
-        axis.text.x=element_text(angle=90, vjust=0.5, hjust=1,size=10,colour="#525252"),
-        axis.title.x=element_text(angle=0,size=12,face="bold",vjust=0,colour="#525252"),
-        axis.title.y=element_text(angle=90, size=12,face="bold",vjust=-2,colour="#525252"),
-        strip.text.x = element_text(size = 9, colour = "#525252", face = "bold"),
-        strip.background = element_blank(),
-        legend.position = "bottom",
-        legend.title = element_text(vjust = 1, size=12,colour="#525252"),
-        legend.text = element_text(size=10,colour="#525252"),
-        legend.key.size = unit(0.6, "cm"))+
-  scale_y_discrete(labels = c("Autonomic ganglia", "Bone", "Breast", "CNS", "Endometrium",
-                              "Blood", "Kidney", "Large intestine", "Liver", "Lung", "Oesophagus",
-                              "Ovary", "Pancreas", "Pleura", "Skin", "Soft tissue", "Stomach", "UADT", "Urinary tract"))
-
-# Confounders plot
-confounders   <- c("ADSL","MAT2A","PRMT5","RPA3")
+# Confounders plot for the top causally inferred hits
+confounders   <- best_causal_hits$sl_partner_gene #c("ADSL","MAT2A","PRMT5","RPA3")
 bp_summary_df <- hits_pancan %>% filter(sl_partner_gene %in% confounders)
-e <- ggplot(bp_summary_df, aes(x=driver_gene, y=sl_partner_gene, size = -log(mut_pvalue, base = 10))) +
+# removing long names for better aesthetics
+bp_summary_df$sl_partner_gene <- unlist(lapply(bp_summary_df$sl_partner_gene,
+                                        function(x){strsplit(x, ",")[[1]][1]}))
+b <- ggplot(bp_summary_df, aes(x=driver_gene, y=sl_partner_gene, size = -log(mut_pvalue, base = 10))) +
   geom_point(fill = "#055C95",color="#055C95", alpha=0.45) +
   scale_size_continuous(range=c(2, 10)) +
   theme_bw() +
@@ -93,18 +79,48 @@ e <- ggplot(bp_summary_df, aes(x=driver_gene, y=sl_partner_gene, size = -log(mut
         legend.text = element_text(size=10,colour="#525252"),
         legend.key.size = unit(0.6, "cm"))
 
-al_plots <- align_plots(c,d,e, align = "v", axis = "l" )
-row_1 <- plot_grid(NULL, NULL, labels = c("A","B"), nrow = 1, rel_widths = c(0.6,0.4), label_size = 13)
-row_2 <- plot_grid(al_plots[[1]], labels = c("C"),  nrow = 1, label_size = 13)
-row_3 <- plot_grid(al_plots[[2]], labels = c("D"),  nrow = 1, label_size = 13)
-row_4 <- plot_grid(al_plots[[3]],NULL, NULL, labels = c("E","","F"), nrow = 1, rel_widths = c(0.6,0.02,0.38),label_size = 13)
-fin_plot <- plot_grid(row_1, row_2, row_3, row_4,
-                      nrow = 4,
+# Heatmap of important hits after causal filtering step
+# removing long names for better aesthetics
+summary_df$sl_partner_gene <- unlist(lapply(summary_df$sl_partner_gene,
+                                               function(x){strsplit(x, ",")[[1]][1]}))
+
+d <- ggplot(summary_df, aes(x = sl_partner_gene, y = canc_type)) +
+  geom_tile(aes(fill = -log(as.numeric(as.character(mut_pvalue))))) +
+  facet_grid(.~ factor(driver_gene), scales = "free_x", space = "free_x") +
+  scale_fill_viridis(option="D", begin = 0, end = 1, alpha = 0.9) +
+  theme_bw() +
+  ylab("Primary sites") +
+  xlab("SL partner genes") +
+  labs(fill = "-log(p-value)")+
+  theme(panel.grid= element_blank(),
+        axis.ticks = element_line(size=0.5,color="#525252"),
+        axis.line = element_line(colour="#525252"),
+        axis.text.y=element_text(angle=0, vjust=0.5, hjust=1,size=10,colour="#525252"),
+        axis.text.x=element_text(angle=90, vjust=0.5, hjust=1,size=10,colour="#525252"),
+        axis.title.x=element_text(angle=0,size=12,face="bold",vjust=0,colour="#525252"),
+        axis.title.y=element_text(angle=90, size=12,face="bold",vjust=-2,colour="#525252"),
+        strip.text.x = element_text(size = 9, angle = 90, colour = "#525252", face = "bold"),
+        strip.background = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(vjust = 1, size=12,colour="#525252"),
+        legend.text = element_text(size=10,colour="#525252"),
+        legend.key.size = unit(0.6, "cm"))+
+  scale_y_discrete(labels = c("Autonomic ganglia", "Bone", "Breast", "CNS", "Endometrium",
+                              "Blood", "Kidney", "Large intestine", "Liver", "Lung", "Oesophagus",
+                              "Ovary", "Pancreas", "Skin", "Soft tissue", "Stomach", "UADT", "Urinary tract")) # No Pleura after causal filtering
+
+# aligning the y axis of all plots
+al_plots <- align_plots(a,b,d, align = "v", axis = "l" )
+row_1 <- plot_grid(al_plots[[1]], labels = c("A"),  nrow = 1, label_size = 13)
+row_2 <- plot_grid(al_plots[[2]], NULL,labels = c("B", "C"),  rel_widths = c(0.75,0.25), nrow = 1, label_size = 13)
+row_3 <- plot_grid(al_plots[[3]], labels = c("D"),  nrow = 1, label_size = 13)
+fin_plot <- plot_grid(row_1, row_2, row_3,
+                      nrow = 3,
                       ncol = 1,
-                      rel_heights = c(0.85,1.1,1, 0.65),
+                      rel_heights = c(1,0.85,1),
                       align = "v",
                       axis = 'l')
 
-ggsave(fin_plot, filename = paste0("/Volumes/beerenwinkel/ssumana/Documents/ETH/CRISPR/SLIDR/Figures/finalPlot_res1",Sys.Date(),".pdf"),
-       width = 16, height = 22)
+ggsave(fin_plot, filename = paste0("/Volumes/bsse_group_beerenwinkel/ssumana/Documents/ETH/CRISPR/SLIDR/Figures/finalPlot_res1",Sys.Date(),".pdf"),
+       width = 20, height = 20)
 
---
+++  Scripts/Res2_plots.R
@@ -0,0 +1,132 @@
+# Plotting figure for cancer specific hits.
+
+library(ggplot2)
+library(dplyr)
+library(cowplot)
+library(viridis)
+library(circlize)
+library(ggplotify)
+
+load("~/Downloads/Slidr_Results/ContCN/ProcessedData.Rdata")
+
+# Reading the hit list for 17 cancers
+hit_files <- list.files("~/Downloads/Slidr_Results/ContCN/Hit_List/", full.names = TRUE)
+hits        <- lapply(hit_files, function(x){read.delim(x, stringsAsFactors = FALSE, sep = "\t")})
+names(hits) <- lapply(hit_files, function(x){sub("SL_hits_", "",strsplit(x, "\\/|\\.")[[1]][9])})
+hits        <- lapply(hits, function(x){ x %>% dplyr::filter(WT_pvalue >= 0.2)})
+hits        <- hits[Primary_sites]
+
+# A vector of all the drivers
+all_drivers  <- unique(unlist(lapply(hits, function(x){x$driver_gene})))
+all_partners <- unique(unlist(lapply(hits, function(x){x$sl_partner_gene})))
+
+# Create a data-frame of mutation frequencies of each driver in each cancer
+mut_freq_df <- do.call(rbind.data.frame, sapply(all_data,
+                                                function(x){
+                                                  cbind(rep(x$primary_site, nrow(x$mutations)),
+                                                        rownames(x$mutations),
+                                                        rowMeans(x$mutations))}))
+colnames(mut_freq_df) <- c("canc_type", "driver_gene", "freq")
+
+# filtering out drivers without an SL partner
+mut_freq_df <- mut_freq_df %>%
+                dplyr::filter(driver_gene %in% all_drivers)
+#mut_freq_df$canc_type <- factor(mut_freq_df$canc_type, levels = sort(levels(mut_freq_df$canc_type)))
+
+# Plotting driver frequencies across cancer types
+a <- ggplot(mut_freq_df, aes(x = driver_gene, y = factor(canc_type)))+
+      geom_tile(aes(fill = as.numeric(as.character(freq))), color = "white") +
+      scale_fill_viridis(option="A", begin = 0, end = 0.95, alpha = 0.9, direction = -1) +
+      theme_bw() +
+      ylab("Primary sites") +
+      xlab("Driver genes") +
+      labs(fill = "Frequency")+
+      theme(panel.grid= element_blank(),
+            axis.ticks = element_line(size=0.5,color="#525252"),
+            axis.line = element_line(colour="#525252"),
+            axis.text.y=element_text(angle=0, vjust=0.5, hjust=1,size=10,colour="#525252"),
+            axis.text.x=element_text(angle=90, vjust=0.5, hjust=1,size=10,colour="#525252"),
+            axis.title.x=element_text(angle=0,size=12,face="bold",vjust=0,colour="#525252"),
+            axis.title.y=element_text(angle=90, size=12,face="bold",vjust=-2,colour="#525252"),
+            strip.text.x = element_text(size = 9, angle = 90, colour = "#525252", face = "bold"),
+            strip.background = element_blank(),
+            legend.position = "bottom",
+            legend.title = element_text(vjust = 1, size=12,colour="#525252"),
+            legend.text = element_text(size=10,colour="#525252"),
+            legend.key.size = unit(0.6, "cm")) +
+  scale_y_discrete(labels = c("Pancreas", "Lung", "Liver", "Breast", "Skin", "Endometrium", "Thyroid", "Stomach",
+                              "Soft tissue", "Bone", "Ovary", "Blood", "Kidney", "CNS", "Oesophagus", "Urinary tract",
+                              "Large intestine"))
+
+
+# Chord diagram
+cs_hits <- read.delim("~/Downloads/Slidr_Results/CanSpecific_literature.txt", stringsAsFactors = FALSE)
+cs_mat <- reshape2::acast(cs_hits, driver_gene~sl_partner_gene, value.var="mut_qvalue")
+cs_mat[is.na(cs_mat)] <- 0
+cs_mat[cs_mat>0]      <- 1
+colnames(cs_mat) <- sapply(colnames(cs_mat), function(x){strsplit(x, ",")[[1]][1]})
+
+color_sites <- c("#CC99BB", "#771155", "#77AADD", "#44AA77",
+                  "#114477", "#DD7788", "#44AAAA", "#774411",
+                  "#DDDD77", "#147C47", "#DDAA77")
+
+color_sites <- viridis(length(unique(cs_hits$Type)), option = "D", alpha = 0.85, begin = 0.1, end = 0.96)
+names(color_sites) <- sort(unique(cs_hits$Type))
+
+color_df <- cbind.data.frame(cs_hits$driver_gene, cs_hits$sl_partner_gene, color_sites[cs_hits$Type])
+
+# Basic chord diagram without the labels
+chordDiagram(cs_mat,
+             annotationTrack = "grid",
+             annotationTrackHeight = 0.03,
+             preAllocateTracks = 1,
+             grid.col = "#525252",
+             grid.border = "#525252",
+             col = color_df)
+# Adding vertical labels track
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y){
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim),
+              ylim[1] + .1,
+              sector.name,
+              facing = "clockwise",
+              niceFacing = TRUE,
+              adj = c(0.05, 0.5),
+              cex = 0.8,
+              col = "#525252")
+  # circos.axis(h = "top",
+  #             labels.cex = 1e-7,
+  #             major.tick = FALSE,
+  #             sector.index = sector.name,
+  #             track.index = 2)
+}, bg.border = NA)
+
+# Converting it into gtable for using plot_grid
+b <- plot_to_gtable(recordPlot())
+b <- as.ggplot(b)
+b <- b +
+      theme_nothing() +
+      theme(aspect.ratio = 1)
+
+# Adding legend: Plotting a density plot to get the legend
+lgd <- get_legend(ggplot(cs_hits, aes(mut_qvalue, color = Type, fill = Type)) +
+                    geom_density(alpha = .85) +
+                    theme(legend.position = "right",
+                          legend.title = element_text(vjust = 1, size=12,colour="#525252"),
+                          legend.text = element_text(size=10,colour="#525252"),
+                          legend.key.size = unit(0.5, "cm"),
+                          legend.key = element_rect(color="white")) +
+                    scale_fill_manual(values = color_sites, name = "Primary site")+
+                    scale_color_manual(values = color_sites, name = "Primary site"))
+
+row_1 <- plot_grid(a, labels = c("A"),  nrow = 1, label_size = 13)
+row_2 <- plot_grid(b, lgd, NULL, labels = c("B","",""), rel_widths = c(0.3,0.0001,0.5), nrow = 1, label_size = 13)
+fin_plot <- plot_grid(row_1, row_2,
+                      nrow = 2,
+                      ncol = 1,
+                      rel_heights = c(1,1.2))
+
+ggsave(fin_plot, filename = paste0("/Volumes/bsse_group_beerenwinkel/ssumana/Documents/ETH/CRISPR/SLIDR/Figures/finalPlot_res2",Sys.Date(),".pdf"),
+       width = 24, height = 12)
