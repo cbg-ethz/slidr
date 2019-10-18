f8f2f27fcaba9ffa312f1309b62a9507528bc209
anamus90 <anamus90@gmail.com>
2019-10-18 16:34:30 +0200

    Created Liver data under data

---
+++ R/liverdata_doc.R
@@ -0,0 +1,14 @@
+#' SLIdR object for liver cancer.
+#'
+#' A dataset containing the viabilities, mutation profiles,
+#' copy number alterations, and  mutation annotation dataframes,
+#' for liver cancer.
+#'
+#' @format A list with 5 data structures:
+#' \describe{
+#'   \item{viabilities}{ATARiS viability scores of 6393 perturbed genes (rows) across 15 cell lines (cols).}
+#'   \item{mutations}{mutation matrix of 7 driver genes (rows) across 13 cell lines (cols).}
+#'   \item{CNalterations}{copy number matrix of 54 genes (rows) across 15 cell lines (cols).}
+#'   \item{CNalterations}{data frame of mutation annotations of all the cell lines for the box plots.}
+#' }
+"LiverData"
