ba5f7494bce36f745d828e816fef4e44e3f477b6
anamus90 <anamus90@gmail.com>
2020-10-21 21:47:13 -0700

    added writing down the results after filtering prism validation hits based on pvalue threshold

--- Scripts/prism_validation.R
+++ Scripts/prism_validation.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-########### Pan-cancer data ###############
+########### Load slidr pred data ###############
 # Load pan cancer object
 load("~/Downloads/Slidr_Results_new/PanCan8pc/pancan.Rdata")
 rm(list=setdiff(ls(), c("all_data", "hits", "pc_data")))
@@ -8,8 +8,6 @@ rm(list=setdiff(ls(), c("all_data", "hits", "pc_data")))
 # Load hits
 pc_hits  <- read.delim2("~/Downloads/Slidr_Results_new/PanCan8pc/Hit_List/SL_hits_pan_cancer.txt",
                         stringsAsFactors = FALSE, header = TRUE)
-# Filter out self-dependent hits
-pc_hits <- dplyr::filter(pc_hits, driver_gene != sl_partner_gene)
 
 ########### Prism screen data ###############
 # Load the primary drug screens data
@@ -36,12 +34,16 @@ drugs_red <- drugs_df %>%dplyr::select(column_name, broad_id, name, target)
 
 # Function to test and validate drug targets for the predicted SL pairs on prism data
 getSigDrugs <- function(canc_data, pred_hits){
+
+  # Filter out self-dependent hits
+  pred_hits <- dplyr::filter(pred_hits, driver_gene != sl_partner_gene)
+
   # retrieving the subset of drugs for the SL partner genes
   subset_drugs <- do.call(rbind.data.frame,
                           sapply(unique(pred_hits$sl_partner_gene),
                                  function(x){drugs_red[which(grepl(x, drugs_red$target)),]}, simplify = FALSE))
 
-  subset_drugs$sl_partner_gene <- sapply(row.names(subset_drugs), function(x){strsplit(x,"[.]")[[1]][1]})
+  subset_drugs$sl_target_gene <- sapply(row.names(subset_drugs), function(x){strsplit(x,"[.]")[[1]][1]})
 
   # initializing variables for the results
   results   <- data.frame()
@@ -51,8 +53,8 @@ getSigDrugs <- function(canc_data, pred_hits){
     target_gene <- pred_hits$sl_partner_gene[i]
     driver_gene <- pred_hits$driver_gene[i]
 
-    if(target_gene %in% subset_drugs$sl_partner_gene){
-      temp <- dplyr::filter(subset_drugs, target_gene == sl_partner_gene)
+    if(target_gene %in% subset_drugs$sl_target_gene){
+      temp <- dplyr::filter(subset_drugs, target_gene == sl_target_gene)
 
       # Stratifying based on driver gene mutation status
       WT_celllines  <- colnames(mutations)[which(mutations[driver_gene,] == 0)]
@@ -66,7 +68,7 @@ getSigDrugs <- function(canc_data, pred_hits){
         mut_viability <- drug_screen[mut_celllines,compound]
         WT_viability  <- drug_screen[WT_celllines,compound]
 
-        # Ensuring that the missing data is less than one third
+        # Ensuring that the missing data is less than half
         if(sum(is.na(mut_viability)) < length(mut_celllines)/2 && sum(is.na(WT_viability)) < length(WT_celllines)/2){
           # Testing if the viability in mutated cell lines is significantly less than in WT from the drug
           p_val <- t.test(mut_viability, WT_viability, alternative =  "less")$p.value
@@ -81,9 +83,53 @@ getSigDrugs <- function(canc_data, pred_hits){
               valid_hits = results))
 }
 
+########### run pan-cancer ###############
 # Validate hits for pancancer data
 pc_valid <- getSigDrugs(pc_data, pc_hits)
 
+res_df <- pc_valid$valid_hits %>%
+            dplyr::filter(p_val < 0.1) %>%
+            dplyr::arrange(p_val) %>%
+            dplyr::select(driver_gene, sl_partner_gene,
+                          WT_pvalue, mut_pvalue,
+                          broad_id, name, target, p_val)
+
+colnames(res_df) <- c("Driver gene","SL partner gene",
+                      "WT pvalue",	"mut pvalue", "Broad ID",
+                      "Drug name", "Drug target(s)", "Validation pvalue")
+
+write.table(x = res_df,
+            file = paste0("~/Downloads/Slidr_Results_new/DepMap/Prism/prism_validation_pancancer.txt"),
+            quote = FALSE,
+            row.names = FALSE,
+            sep = "\t")
+
+########### run cancer type-specific ###############
 # Validate hits for cancer-type specific
 cs_valid <- apply(mapply(getSigDrugs, all_data, hits), 2, as.list)
 
+for(tissue in names(cs_valid)){
+
+  res_df <- cs_valid[[tissue]]$valid_hits
+
+  if(nrow(res_df) > 0){
+    res_df <- res_df %>%
+                dplyr::filter(p_val < 0.1) %>%
+                dplyr::arrange(p_val) %>%
+                dplyr::select(driver_gene, sl_partner_gene,
+                              WT_pvalue, mut_pvalue,
+                              broad_id, name, target, p_val)
+
+    colnames(res_df) <- c("Driver gene","SL partner gene",
+                          "WT pvalue",	"mut pvalue", "Broad ID",
+                          "Drug name", "Drug target(s)", "Validation pvalue")
+    write.table(x = res_df,
+                file = paste0("~/Downloads/Slidr_Results_new/DepMap/Prism/prism_validation_", tissue, ".txt"),
+                quote = FALSE,
+                row.names = FALSE,
+                sep = "\t")
+  }
+
+}
+
+save.image("~/Downloads/Slidr_Results_new/DepMap/Prism/prism_validation.Rdata")
