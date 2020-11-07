07471b9d5d8b8f50d4b5d2e0d30381b5bcc639b1
anamus90 <anamus90@gmail.com>
2020-11-06 20:19:02 -0800

    Modified to include median centering before imputing and to run it on both pancancer and cancertype specific

--- Scripts/slidr_crispr_run.R
+++ Scripts/slidr_crispr_run.R
@@ -9,42 +9,67 @@ load("./PanCan8pc/pancan.Rdata")
 rm(list=setdiff(ls(), c("all_data", "hits", "pc_data")))
 
 # Load Crispr data from depmap
-crispr_dat <- read.csv("./DepMap/Achilles_gene_effect.csv",
+crispr_data <- read.csv("./DepMap/Achilles_gene_effect.csv",
                         header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
 
 # Sample information
 sample_info <- read.csv("./DepMap/sample_info.csv",
                         header = TRUE, stringsAsFactors = FALSE)
 
-# Dataframe with cell line names for crispr data
-temp <- crispr_dat %>%
-            left_join(select(sample_info, DepMap_ID, CCLE_Name), by = "DepMap_ID")
+# Processed dataframe with cell line names for crispr data
+proc_data <- crispr_data %>%
+              left_join(select(sample_info, DepMap_ID, CCLE_Name), by = "DepMap_ID")
 
-rownames(temp)   <- tolower(temp$CCLE_Name)
-colnames(temp)   <- sapply(colnames(temp), function(x){strsplit(x, " ")[[1]][1]})
+rownames(proc_data)   <- tolower(proc_data$CCLE_Name)
+colnames(proc_data)   <- sapply(colnames(proc_data), function(x){strsplit(x, " ")[[1]][1]})
+
+# Median centering the data
+proc_data <- t(proc_data[-c(1,18121)])
+proc_data <- t(apply(proc_data, 1, function(x){x - median(x, na.rm = TRUE)}))
 
 # imputing missing values
-temp <- t(temp[-c(1,18121)])
-temp <- as.data.frame(t(apply(temp,
+proc_data <- as.data.frame(t(apply(proc_data,
                       1,
                       function(x){
                         x[which(is.na(x))] <- mean(x, na.rm = TRUE)
                         x})))
 
-# Preparing slidr object for crispr data
-crispr_pc_data   <- pc_data
-common_KO        <- intersect(rownames(pc_data$viabilities), rownames(temp))
-common_celllines <- intersect(colnames(pc_data$viabilities), colnames(temp))
+# Function for preparing slidr object for crispr data
+getCRISPRdata <- function(slidr_obj){
 
-# Subset of common cell lines and genes for viability, mutatins and copy number
-crispr_pc_data$viabilities   <- temp[common_KO, common_celllines]
-crispr_pc_data$mutations     <- crispr_pc_data$mutations[ ,common_celllines]
-crispr_pc_data$CNalterations <- crispr_pc_data$CNalterations[ ,common_celllines]
+  crispr_so <- slidr_obj
 
-# running slidr on crispr data
-path_results <- "./DepMap/"
+  common_KO        <- intersect(rownames(slidr_obj$viabilities), rownames(proc_data))
+  common_celllines <- intersect(colnames(slidr_obj$mutations), colnames(proc_data))
+
+  # Subset of common cell lines and genes for viability, mutations and copy number
+  crispr_so$viabilities   <- proc_data[common_KO, common_celllines]
+  crispr_so$mutations     <- crispr_so$mutations[ ,common_celllines]
+  crispr_so$CNalterations <- crispr_so$CNalterations[ ,common_celllines]
+
+  return(crispr_so)
+}
+
+# Getting slidr object for cancer type and cancer-type specific
+crispr_pc_data <- getCRISPRdata(pc_data)
+
+crispr_cs_data <- lapply(all_data, getCRISPRdata)
+
+# running slidr on crispr data for pancancer data
+path_results <- "./DepMap/Crispr/PanCan/"
 
 hits_crispr_pc <- slidr::identifySLHits(canc_data = crispr_pc_data,
                                         path_results = path_results,
-                                        WT_pval_thresh = 1e-6,
+                                        WT_pval_thresh = 0,
                                         fp_thresh = 1)
+
+
+# Running slidr on crispr data for cancer type specific
+path_results <- "./DepMap/Crispr/CancerSpecific/"
+
+hits_crispr_cs <- lapply(crispr_cs_data[1],
+                         function(x){slidr::identifySLHits(canc_data = x,
+                                                           path_results = path_results,
+                                                           WT_pval_thresh = 0.1)})
+
+names(hits_crispr_cs) <- names(crispr_cs_data)
